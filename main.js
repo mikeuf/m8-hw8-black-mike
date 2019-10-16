5a61411c6b15672a2e7fc21b68de67e9db5bfb52
Mike <mblack101@ufl.edu>
2019-10-15 23:37:44 -0400

    Add javascript event listeners to menu

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // It is always helpful to use comments in your code!
+
+
+var mainMenuLabels = document.querySelectorAll("main-menu-labels");
+
+mainMenuLabels.forEach(function(element) {
+
+element.addEventListener("mouseover", function() {
+element.style.display = "none";
+});
+
+
+element.addEventListener("mouseout", function() {
+element.style.display = "block";
+});
+
+
+});
